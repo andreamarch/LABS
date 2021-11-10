c36e2cac9180162fce4e088012a97d5f5d61c8e6
Andrea <s290212@studenti.polito.it>
2021-11-10 10:41:29 +0100

    adding comments to lab4 classes

--- LAB4/LAB4.py
+++ LAB4/LAB4.py
@@ -39,11 +39,14 @@ class Node:
 
     def node_propagate(self, signalinformation):
         flag = 0
-        if len(signalinformation.path) != 1:
+        if len(signalinformation.path) != 1:  # if not at the last node of the path
+            # save the next line label
             line_label = signalinformation.path[0] + signalinformation.path[1]
             flag = 1
+        # update the path deleting the current node
         signalinformation.update_node(self)
         if flag == 1:
+            # calling the next line propagate method
             self.successive[line_label].line_propagate(signalinformation)
         return signalinformation
 
@@ -56,20 +59,28 @@ class Line:
         self.status = 'occupied'
 
     def line_propagate(self, signalinformation):
+        # save the successive node label
         node_label = self.label[1]
+        # generate the noise acquired on this line
         self.noise_generation(signalinformation)
+        # generate the latency acquired on this line
         self.latency_generation(signalinformation)
+        # call the successive node
         self.successive[node_label].node_propagate(signalinformation)
         return signalinformation
 
     def latency_generation(self, signalinformation):
+        # compute the latency
         speed = consts.c * 2 / 3
         new_latency = self.length / speed
+        # modify the latency in the signal information
         signalinformation.update_latency(new_latency)
         return signalinformation
 
     def noise_generation(self, signalinformation):
+        # compute noise
         new_noise = 1e-9 * signalinformation.signal_power * self.length
+        # modify noise in the signal information
         signalinformation.update_noise_pow(new_noise)
         return signalinformation
 
@@ -80,34 +91,41 @@ class Network:
         self.nodes = dict()
         self.lines = dict()
         self.graph = dict()
+        # open the json file
         with open('nodes.json') as json_file:
             nodes = json.load(json_file)
+        # initiate the Node instances from the json file
         for i, j in nodes.items():
             self.nodes[i] = Node(i, j)
             temp_dict = dict()
-            for x in self.nodes[i].connected_nodes:
+            for x in self.nodes[i].connected_nodes:  # initiating a graph dictionary for the Dijkstra's algorithm
                 temp_dict[x] = 1
             self.graph[i] = temp_dict
 
-        for curr_node, i in nodes.items():
-            for next_node in i['connected_nodes']:
+        for curr_node, i in nodes.items():  # cycling on the values of attribute nodes
+            for next_node in i['connected_nodes']:  # cycling on the 'connected_nodes' key of the current value
                 line_label = curr_node + next_node
+                # save the positions of the current and next nodes
                 x1 = self.nodes[curr_node].position[0]
                 y1 = self.nodes[curr_node].position[1]
                 x2 = self.nodes[next_node].position[0]
                 y2 = self.nodes[next_node].position[1]
+                # compute the length of the line
                 line_length = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+                # save the length as attribute of the instance of the line
                 self.lines[line_label] = Line(line_label, line_length)
 
     def connect(self):
         for node_label, node_value in self.nodes.items():
             for line_label, line_value in self.lines.items():
-                if line_label[0] == node_label:
+                if line_label[0] == node_label:  # while cycling on the lines, if we have that the first node of the
+                    # line corresponds to the current node, save the line in the successive of that node
                     node_value.successive[line_label] = line_value
                 for i, j in self.nodes.items():
-                    if i == line_label[1]:
+                    if i == line_label[1]:  # while cycling on the nodes, if the line corresponds to the second
+                        # element of the line, save the node in the successive of the current line
                         line_value.successive[i] = j
-        self.weighted_lines = weigthed_nodes_build(self)
+        self.weighted_lines = weigthed_nodes_build(self)  # build the data frame
         return
 
     # implementing Dijkstra's algorithm in order to find all paths
@@ -115,7 +133,7 @@ class Network:
         strt = strt.upper()
         end = end.upper()
         path = path + [strt]
-        if strt == end:
+        if strt == end:  # we reached the end
             return [path]
         if strt not in self.graph:
             return []
@@ -129,10 +147,10 @@ class Network:
 
     def find_best_snr(self, strt, end):
         from_to = strt.upper() + '-' + end.upper()
-        best_snr = 0
+        best_snr = 0  # set to zero because it has to find the highest value of snr
         best_path = ''
         for row in self.weighted_lines.itertuples():
-            if row.FromTo == from_to:
+            if row.FromTo == from_to: # using the FromTo column of the data structure weighted_lines
                 if row.SNR > best_snr:
                     path = list(row.Path.replace('->', ''))
                     flag = True
@@ -147,10 +165,10 @@ class Network:
 
     def find_best_latency(self, strt, end):
         from_to = strt.upper() + '-' + end.upper()
-        best_latency = 1e99
+        best_latency = 1e99  # set to infinite because it has to find the lowest value of latency
         best_path = ''
         for row in self.weighted_lines.itertuples():
-            if row.FromTo == from_to:
+            if row.FromTo == from_to:  # using the FromTo column of the data structure weighted_lines
                 if row.Latency < best_latency:
                     path = list(row.Path.replace('->', ''))
                     flag = True
@@ -165,21 +183,21 @@ class Network:
 
     def propagate(self, signalinformation):
         first_node = signalinformation.path[0]
-        self.nodes[first_node].node_propagate(signalinformation)
+        self.nodes[first_node].node_propagate(signalinformation)  # calling the propagate method for the first node
         return signalinformation
 
     def draw_network(self):
         x = list()
         y = list()
         label = list()
-        cnt = 0
         for i, j in self.nodes.items():
+            # saving labels in vector
             label.append(i)
+            # saving positions in vector
             xi = float(j.position[0]) * 1e-5
             x.append(xi)
             yi = float(j.position[1]) * 1e-5
             y.append(yi)
-
         fig, ax = plt.subplots(1)
         ax.set_aspect('equal')
         plt.axis([-5, 5, -5, 5.5])
@@ -191,12 +209,12 @@ class Network:
                         indx2 = label.index(k)
                         xx = [x[indx2], x[indx]]
                         yy = [y[indx2], y[indx]]
-                        plt.plot(xx, yy, 'k-.', linewidth=0.5)
+                        plt.plot(xx, yy, 'k-.', linewidth=0.5)  # lines
         for xx, yy, s in zip(x, y, label):
-            circ = plt.Circle((xx, yy), radius=0.5)
+            circ = plt.Circle((xx, yy), radius=0.5)  # circles
             circ.set_facecolor('c')
             circ.set_edgecolor('k')
-            ax.txt = plt.text(xx - 0.13, yy - 0.13, s, fontsize=14)
+            ax.txt = plt.text(xx - 0.13, yy - 0.13, s, fontsize=14)  # labels
             ax.add_patch(circ)
         # Show the image
         plt.show()
@@ -209,24 +227,24 @@ class Network:
             if lat_or_snr == 'latency':
                 best_lat_path = self.find_best_latency(inp, outp)[1]
                 if best_lat_path != '':  # if an available path was found, the string shouldn't be empty
-                    signal = SignalInformation(i.signal_power, best_lat_path)
-                    signal = (self.propagate(signal))
+                    signal = SignalInformation(i.signal_power, best_lat_path)  # initiate the SignalInfo instance
+                    signal = (self.propagate(signal))  # propagate the signal info through the path
                     i.snr = 10 * np.log10(signal.signal_power / signal.noise_power)
                     i.latency = signal.latency
-                else:
+                else:  # no lines available between these nodes
                     i.snr = 0
                     i.latency = None
             elif lat_or_snr == 'snr':
                 best_snr_path = self.find_best_snr(inp, outp)[1]
-                if best_snr_path != '':
-                    signal = SignalInformation(i.signal_power, best_snr_path)
-                    signal = (self.propagate(signal))
+                if best_snr_path != '':  # if an available path was found, the string shouldn't be empty
+                    signal = SignalInformation(i.signal_power, best_snr_path)  # initiate the SignalInfo instance
+                    signal = (self.propagate(signal))  # propagate the signal info through the path
                     i.snr = 10 * np.log10(signal.signal_power / signal.noise_power)
                     i.latency = signal.latency
-                else:
+                else:  # no lines available between these nodes
                     i.snr = 0
                     i.latency = None
-            else:
+            else:  # wrong string was passed
                 print('Please choose between strings "snr" and "latency".')
         return
 
@@ -239,14 +257,14 @@ def weigthed_nodes_build(network, power = 1e-3):
     latency = []
     path_vector = []
     snr = []
-    for i in network.nodes.keys():
+    for i in network.nodes.keys():  # building vector of paths
         for j in network.nodes.keys():
             if j != i:
                 paths.append(network.find_paths(i, j))
 
     for i in range(0, len(paths)):
         for j in range(0, len(paths[i])):
-            strng = ''
+            strng = ''  #  building the string in the format with ->
             for k in range(0, len(paths[i][j])):
                 strng += paths[i][j][k]
                 if k < len(paths[i][j]) - 1:
@@ -276,6 +294,7 @@ class Connection:
 
 network = Network()
 network.connect()
+network.draw_network()
 nodes = list(network.nodes.keys())
 connections = []
 signal_power = 1e-3
