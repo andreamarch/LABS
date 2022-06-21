cd3a1f3f15f5accfeac177baf1a8567657c3af14
Andrea Marchisio <s290212@studenti.polito.it>
2022-06-21 17:26:29 +0200

    Lab 10 point 1 completed (with plots)

--- Tasks/LAB10/LAB10.py
+++ Tasks/LAB10/LAB10.py
@@ -15,11 +15,7 @@ file_fixed = in_directory / 'nodes_full_fixed_rate.json'
 file_flex = in_directory / 'nodes_full_flex_rate.json'
 file_shannon = in_directory / 'nodes_full_shannon.json'
 
-# output file
-outp_plot_snr = out_directory / '9d5_snr.png'
-outp_plot_lat = out_directory / '9d5_lat.png'
-outp_plot_br = out_directory / '9d5_br.png'
-
+# simulation flags
 save_my_figure = False
 verbose = False
 do_simulations = [True, True]
@@ -33,6 +29,17 @@ network_shan = el.Network(file_shannon)
 network_shan.connect()
 
 # ----------------- POINT 1 (Single Traffic Matrix) -----------------
+# output file paths
+outp_plot_avg_snr = out_directory / 'Point1' / '10d1_average_snr'
+outp_plot_min_snr = out_directory / 'Point1' / '10d1_min_snr'
+outp_plot_max_snr = out_directory / 'Point1' / '10d1_max_snr'
+outp_plot_capacity = out_directory / 'Point1' / '10d1_capacity'
+outp_plot_avg_br = out_directory / 'Point1' / '10d1_average_bit_rate'
+outp_plot_min_br = out_directory / 'Point1' / '10d1_min_bit_rate'
+outp_plot_max_br = out_directory / 'Point1' / '10d1_max_bit_rate'
+outp_plot_block_events = out_directory / 'Point1' / '10d1_blocking_events'
+outp_plot_connections = out_directory / 'Point1' / '10d1_successful_connections'
+
 if do_simulations[0]:
     connections_fixed_per_M = []
     number_connections_fixed_rate_per_M = []
@@ -207,8 +214,7 @@ if do_simulations[0]:
         traffic_matrix_shan = utls.generate_traffic_matrix(network_shan, M)
         [connection_tm_shan, saturation_shan] = network_shan.deploy_traffic_matrix(traffic_matrix_shan)
         [successful_connections, blocking_events] = utls.compute_successful_blocking_events(connection_tm_shan)
-        [total_capacity, avg_bit_rate, max_br, min_br] = utls.compute_network_capacity_and_avg_bit_rate(
-            connection_tm_shan)
+        [total_capacity, avg_bit_rate, max_br, min_br] = utls.compute_network_capacity_and_avg_bit_rate(connection_tm_shan)
         [avg_snr, max_snr, min_snr] = utls.compute_average_max_min_snr(connection_tm_shan)
         print()
         print('SHANNON TRANSCEIVER STRATEGY')
@@ -219,7 +225,7 @@ if do_simulations[0]:
             if saturation_shan:
                 print('Shannon network for M =', M, ': network was saturated.')
             elif not saturation_shan:
-                print('Shanno network for M =', M, ': traffic matrix was saturated')
+                print('Shannon network for M =', M, ': traffic matrix was saturated')
         connections_shannon_per_M.append(successful_connections + blocking_events)
         number_connections_shannon_per_M.append(successful_connections)
         number_blocking_events_shannon_per_M.append(blocking_events)
@@ -253,6 +259,115 @@ if do_simulations[0]:
         print('Average minimum bit rate at run', counter + 1, '=', average_min_bit_rate_shannon, 'Gbps')
         print('Average maximum bit rate at run', counter + 1, '=', average_max_bit_rate_shannon, 'Gbps')
         print('Average number of blocking events at run', counter + 1, '=', average_blocking_events_shannon)
-        print('Average number of successful connections at run', counter + 1, '=', average_successful_connections_shannon)
+        print('Average number of successful connections at run', counter + 1, '=',
+              average_successful_connections_shannon)
         utls.free_lines_and_switch_matrix(file_shannon, network_shan)
 
+        if counter == 0:
+            lat_fixed = [connection.latency for connection in connection_tm_fixed]
+            lat_flex = [connection.latency for connection in connection_tm_flex]
+            lat_shan = [connection.latency for connection in connection_tm_shan]
+            lat_list = [lat_fixed, lat_flex, lat_shan]
+
+            snr_fixed = [connection.snr for connection in connection_tm_fixed]
+            snr_flex = [connection.snr for connection in connection_tm_flex]
+            snr_shan = [connection.snr for connection in connection_tm_shan]
+            snr_list = [snr_fixed, snr_flex, snr_shan]
+
+            br_fixed = [connection.bit_rate for connection in connection_tm_fixed]
+            br_flex = [connection.bit_rate for connection in connection_tm_flex]
+            br_shan = [connection.bit_rate for connection in connection_tm_shan]
+            br_list = [br_fixed, br_flex, br_shan]
+
+            utls.plot_histogram(1, snr_list, 20, 'k', ['r', 'b', 'g'], ['Fixed rate', 'Flexible rate', 'Shannon rate'],
+                                'Single Matrix analysis - SNR distribution, M = ' + str(M), 'Number of results',
+                                'SNR, dB')
+            if save_my_figure:
+                plt.savefig(outp_plot_snr)
+
+            utls.plot_histogram(2, lat_list, 20, 'k', ['r', 'b', 'g'], ['Fixed rate', 'Flexible rate', 'Shannon rate'],
+                                'Single Matrix analysis - Latency, M = ' + str(M), 'Number of results', 'Latency, s')
+            if save_my_figure:
+                plt.savefig(outp_plot_lat)
+
+            utls.plot_histogram(3, br_list, 15, 'k', ['r', 'b', 'g'], ['Fixed rate', 'Flexible rate', 'Shannon rate'],
+                                'Single Matrix analysis - Bit rate, M = ' + str(M), 'Number of results',
+                                'Bit rate, Gbps')
+            if save_my_figure:
+                plt.savefig(outp_plot_br)
+
+    string_for_titles = 'M = ' + str(M_list[0]) + ' and ' + str(len(M_list)) + ' runs'
+    utls.plot_bar(figure_num=4, list_data=[[average_average_snr_fixed], [average_average_snr_flex],
+                                           [average_average_snr_shannon]],
+                  bbox_to_anchor=(0.5, -0.35), bottom=0.25, loc='lower center', edge_color='k', color=['r', 'b', 'g'],
+                  label=['Fixed Rate', 'Flex Rate', 'Shannon Rate'], xlabel='', ylabel='SNR, dB',
+                  title='Single Matrix analysis - Average SNR, ' + string_for_titles, myalpha=1)
+    if save_my_figure:
+        plt.savefig(outp_plot_avg_snr)
+
+    utls.plot_bar(figure_num=5, list_data=[[average_min_snr_fixed], [average_min_snr_flex],
+                                           [average_min_snr_shannon]],
+                  bbox_to_anchor=(0.5, -0.35), bottom=0.25, loc='lower center', edge_color='k', color=['r', 'b', 'g'],
+                  label=['Fixed Rate', 'Flex Rate', 'Shannon Rate'], xlabel='', ylabel='SNR, dB',
+                  title='Single Matrix analysis - Minimum SNR, ' + string_for_titles, myalpha=1)
+    if save_my_figure:
+        plt.savefig(outp_plot_min_snr)
+
+    utls.plot_bar(figure_num=6, list_data=[[average_max_snr_fixed], [average_max_snr_flex],
+                                           [average_max_snr_shannon]],
+                  bbox_to_anchor=(0.5, -0.35), bottom=0.25, loc='lower center', edge_color='k', color=['r', 'b', 'g'],
+                  label=['Fixed Rate', 'Flex Rate', 'Shannon Rate'], xlabel='', ylabel='SNR, dB',
+                  title='Single Matrix analysis - Maximum SNR, ' + string_for_titles, myalpha=1)
+    if save_my_figure:
+        plt.savefig(outp_plot_max_snr)
+
+    utls.plot_bar(figure_num=7, list_data=[[average_total_capacity_fixed / 1000], [average_total_capacity_flex / 1000],
+                                           [average_total_capacity_shannon / 1000]],
+                  bbox_to_anchor=(0.5, -0.35), bottom=0.25, loc='lower center', edge_color='k', color=['r', 'b', 'g'],
+                  label=['Fixed Rate', 'Flex Rate', 'Shannon Rate'], xlabel='', ylabel='Capacity, Tbps',
+                  title='Single Matrix analysis - Total capacity, ' + string_for_titles, myalpha=1)
+    if save_my_figure:
+        plt.savefig(outp_plot_capacity)
+
+    utls.plot_bar(figure_num=8, list_data=[[average_average_bit_rate_fixed], [average_average_bit_rate_flex],
+                                           [average_average_bit_rate_shannon]],
+                  bbox_to_anchor=(0.5, -0.35), bottom=0.25, loc='lower center', edge_color='k', color=['r', 'b', 'g'],
+                  label=['Fixed Rate', 'Flex Rate', 'Shannon Rate'], xlabel='', ylabel='Bit rate, Gbps',
+                  title='Single Matrix analysis - Average bit rate, ' + string_for_titles, myalpha=1)
+    if save_my_figure:
+        plt.savefig(outp_plot_avg_br)
+
+    utls.plot_bar(figure_num=9, list_data=[[average_min_bit_rate_fixed], [average_min_bit_rate_flex],
+                                           [average_min_bit_rate_shannon]],
+                  bbox_to_anchor=(0.5, -0.35), bottom=0.25, loc='lower center', edge_color='k', color=['r', 'b', 'g'],
+                  label=['Fixed Rate', 'Flex Rate', 'Shannon Rate'], xlabel='', ylabel='Bit rate, Gbps',
+                  title='Single Matrix analysis - Minimum bit rate, ' + string_for_titles, myalpha=1)
+    if save_my_figure:
+        plt.savefig(outp_plot_min_br)
+
+    utls.plot_bar(figure_num=10, list_data=[[average_max_bit_rate_fixed], [average_max_bit_rate_flex],
+                                            [average_max_bit_rate_shannon]],
+                  bbox_to_anchor=(0.5, -0.35), bottom=0.25, loc='lower center', edge_color='k', color=['r', 'b', 'g'],
+                  label=['Fixed Rate', 'Flex Rate', 'Shannon Rate'], xlabel='', ylabel='Bit rate, Gbps',
+                  title='Single Matrix analysis - Maximum bit rate, ' + string_for_titles, myalpha=1)
+    if save_my_figure:
+        plt.savefig(outp_plot_max_br)
+
+    utls.plot_bar(figure_num=11, list_data=[[average_blocking_events_fixed], [average_blocking_events_flex],
+                                            [average_blocking_events_shannon]],
+                  bbox_to_anchor=(0.5, -0.35), bottom=0.25, loc='lower center', edge_color='k', color=['r', 'b', 'g'],
+                  label=['Fixed Rate', 'Flex Rate', 'Shannon Rate'], xlabel='', ylabel='Number of blocking events',
+                  title='Single Matrix analysis - Blocking Events, ' + string_for_titles, myalpha=1)
+    if save_my_figure:
+        plt.savefig(outp_plot_block_events)
+
+    utls.plot_bar(figure_num=12, list_data=[[average_successful_connections_fixed],
+                                            [average_successful_connections_flex],
+                                            [average_successful_connections_shannon]],
+                  bbox_to_anchor=(0.5, -0.35), bottom=0.25, loc='lower center', edge_color='k', color=['r', 'b', 'g'],
+                  label=['Fixed Rate', 'Flex Rate', 'Shannon Rate'], xlabel='', ylabel='Number of connections',
+                  title='Single Matrix analysis - Successful connections, ' + string_for_titles, myalpha=1)
+    if save_my_figure:
+        plt.savefig(outp_plot_connections)
+
+    plt.show()
